42f6832f667777dc34e632165766d4574f9583ec
Renato Novais <renato.novais.silva@hotmail.com>
2026-08-17 22:40:00 -0300

    feat(config): add JAX-RS activator and persistence configuration

---
+++ src/main/java/br/com/renatonovais/urlshortener/config/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package br.com.renatonovais.urlshortener.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsActivator extends Application {
+}
